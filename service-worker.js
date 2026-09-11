f69634f5baab0807a523bd3fa0a15008b53b5629
Ian Siqueira Racosta <113894395+iangta10@users.noreply.github.com>
2026-09-11 18:06:41 -0300

    Publica Minha Compra PWA

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='minha-compra-v2';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./assets/app.1.b64','./assets/app.2.b64','./assets/app.3.b64','./assets/app.4.b64','./assets/app.5.b64','./assets/app.6.b64'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp;}).catch(()=>caches.match('./index.html')))));
