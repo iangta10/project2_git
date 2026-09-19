19559220422bea2a69f4ad2e09d464764185dc4a
Ian Siqueira Racosta <113894395+iangta10@users.noreply.github.com>
2026-09-19 12:24:51 -0300

    Atualiza cache do Minha Compra v3

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='minha-compra-v2';
+const CACHE='minha-compra-v3';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./assets/app.1.b64','./assets/app.2.b64','./assets/app.3.b64','./assets/app.4.b64','./assets/app.5.b64','./assets/app.6.b64'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
