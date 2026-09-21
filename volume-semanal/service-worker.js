c3cde6776f57fef8502153a60ead0243b1633fb7
Ian Siqueira Racosta <113894395+iangta10@users.noreply.github.com>
2026-09-21 08:02:33 -0300

    feat: add offline service worker

---
+++ volume-semanal/service-worker.js
@@ -0,0 +1,12 @@
+const CACHE="volume-semanal-v2";
+const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon.svg"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith("volume-semanal-")&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET") return;
+  e.respondWith(fetch(e.request).then(resp=>{
+    const copy=resp.clone();
+    caches.open(CACHE).then(c=>c.put(e.request,copy));
+    return resp;
+  }).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))));
+});
